b3352946adc3177ddee68e2f50962846d3b39b94
devmuturi <muturinjonge09@gmail.com>
2026-01-25 23:19:31 +0300

    Add API based architecture

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  respond_to :html, :xml, :json
-
   def index
-    @tasks = Task.all
-    respond_with(@tasks)
+    tasks = Task.all
+    render status: :ok, json: { tasks: }
   end
 end
