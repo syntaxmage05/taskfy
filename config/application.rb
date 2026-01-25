cbd4168663921538bc09bda9f892a5b1e0431ae0
devmuturi <muturinjonge09@gmail.com>
2026-01-25 10:41:10 +0300

    Add React.js to the project

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module Granite
     config.generators do |g|
       g.test_framework :test_unit, fixture: false
     end
+
+    # Configure asset pipeline to use Sprockets
+    config.assets.pipeline = :sprockets
   end
 end
