c528ebc176b5b1e659444f9772f44738cc43ab16
devmuturi <muturinjonge09@gmail.com>
2026-01-28 23:59:19 +0300

    Implement create action for task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const fetch = () => axios.get("/tasks");
 
-const tasksApi = { fetch };
+const create = payload =>
+  axios.post("/tasks", {
+    task: payload,
+  });
+
+const tasksApi = { fetch, create };
 
 export default tasksApi;
