80139ee7c370f5fd9714c88055294b024406807f
devmuturi <muturinjonge09@gmail.com>
2026-01-28 23:59:19 +0300

    Implement create action for task

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CreateTask from "components/Tasks/Create";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
@@ -7,8 +8,7 @@ import Dashboard from "./components/Dashboard";
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" render={() => <div>Home</div>} />
-      <Route exact path="/about" render={() => <div>About</div>} />
+      <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
     </Switch>
   </Router>
