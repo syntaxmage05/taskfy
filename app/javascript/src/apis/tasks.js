8ac62a75fec0d86a0598a40c07ab810fe43858a9
devmuturi <muturinjonge09@gmail.com>
2026-01-29 21:05:54 +0300

    Added show page for a task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -2,11 +2,17 @@ import axios from "axios";
 
 const fetch = () => axios.get("/tasks");
 
+const show = slug => axios.get(`/tasks/${slug}`);
+
 const create = payload =>
-  axios.post("/tasks", {
+  axios.post("/tasks/", {
     task: payload,
   });
 
-const tasksApi = { fetch, create };
+const tasksApi = {
+  fetch,
+  show,
+  create,
+};
 
 export default tasksApi;
