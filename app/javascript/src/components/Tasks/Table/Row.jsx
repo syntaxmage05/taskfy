a15de9d59c7d8a99fb9b9b2a620ad80fbf633582
devmuturi <muturinjonge09@gmail.com>
2026-01-29 21:42:40 +0300

    Add ability to delete a task

--- app/javascript/src/components/Tasks/Table/Row.jsx
+++ app/javascript/src/components/Tasks/Table/Row.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Tooltip } from "components/commons";
 import PropTypes from "prop-types";
 
-const Row = ({ data, showTask }) => (
+const Row = ({ data, showTask, destroyTask }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map(rowData => (
       <tr key={rowData.id}>
-        <td className="border-r border-gray-300 px-4 py-2.5 text-sm font-medium capitalize">
+        <td className="space-x-5 border-r border-gray-300 px-4 py-2.5 text-sm font-medium capitalize">
           <Tooltip tooltipContent={rowData.title}>
             <span>{rowData.title}</span>
           </Tooltip>
@@ -17,6 +17,18 @@ const Row = ({ data, showTask }) => (
             Show
           </a>
         </td>
+        <td
+          className="cursor-pointer px-6 py-4 text-right
+            text-sm font-medium leading-5"
+        >
+          <a
+            className="text-red-500
+              hover:text-red-700"
+            onClick={() => destroyTask(rowData.slug)}
+          >
+            Delete
+          </a>
+        </td>
       </tr>
     ))}
   </tbody>
@@ -24,6 +36,8 @@ const Row = ({ data, showTask }) => (
 
 Row.propTypes = {
   data: PropTypes.array.isRequired,
+  destroyTask: PropTypes.func,
+  showTask: PropTypes.func,
 };
 
 export default Row;
